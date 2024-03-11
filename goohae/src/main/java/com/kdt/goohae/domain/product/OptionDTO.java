37079f22fb15fc5f6497bbe4d54a0c59817d396c
yujeonghyeon <dbdb1114@naver.com>
2024-03-11 22:10:17 +0900

    MultipartFile 필드 추가

--- goohae/src/main/java/com/kdt/goohae/domain/product/OptionDTO.java
+++ goohae/src/main/java/com/kdt/goohae/domain/product/OptionDTO.java
@@ -1,12 +1,14 @@
 package com.kdt.goohae.domain.product;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class OptionDTO {
     private int id;
-    private int product_id;
+    private int productId;
     private String name;
     private int surcharge;
-    private String image_url;
+    private String imageUrl;
+    private MultipartFile imageFile;
 }
