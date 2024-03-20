f170fbfacf6e22d9268e3ca34fa809de3fd50efa
yujeonghyeon <dbdb1114@naver.com>
2024-03-20 19:00:21 +0900

    중복로그인 방지 Interceptor 설정

---
+++ goohae/src/main/java/com/kdt/goohae/config/UserDupleLoginInterceptor.java
@@ -0,0 +1,47 @@
+package com.kdt.goohae.config;
+
+import java.util.Objects;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@RequiredArgsConstructor
+public class UserDupleLoginInterceptor implements HandlerInterceptor {
+
+
+    private final RedisTemplate redisTemplate;
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                            ModelAndView modelAndView) throws Exception {
+
+        String loginId = request.getParameter("id");
+        HttpSession session = request.getSession();
+
+        if(request.getMethod().toLowerCase().equals("post")
+                && loginId.equals(session.getAttribute("loginId"))){
+            ValueOperations valueOperations = redisTemplate.opsForValue();
+
+            if(redisTemplate.hasKey(loginId)){
+                log.info(loginId + " : 중복로그인 입니다. 기존 로그인은 로그아웃 됩니다.");
+                request.getSession().setAttribute("message", "중복로그인 입니다. 기존 로그인은 로그아웃 됩니다.");
+                redisTemplate.delete(loginId);
+            }
+
+            valueOperations.set(loginId, session.getId());
+        }
+
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+//"9BE2F96DF0804BAE883DAF995D68FB28";
+//
+}
